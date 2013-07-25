f18788a67526af0f3c960c18d7565ee99ddda644
abaddonGIT <abaddongit@gmail.com>
2013-07-25 16:44:50 +0400

    new

---
+++ js/goodImage.js
@@ -0,0 +1,6 @@
+/******************************************************
+ * Copyright 2013 by Abaddon <abaddongit@gmail.com>
+ * @author Abaddon <abaddongit@gmail.com>
+ * @version 1.0.0
+ * ***************************************************/
+/*global window, $, jQuery, document */
