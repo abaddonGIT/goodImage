47461cb64a7206d9f58b857aa22f1250a9c82436
abaddonGIT <abaddongit@gmail.com>
2013-07-25 23:31:56 +0400

    1

--- js/goodImage.js
+++ js/goodImage.js
@@ -7,15 +7,33 @@
 (function ($) {
     "use strict";
     
-    $.fn.miniImage = function (options) {
+    $.fn.goodImage = function (options) {
         var def = {
-            
+            'width': 600,
+            'maxWidth': 600,
+            'minWidth': 100,
+            'padding': 5
         };
         
         $.extend(def, options);
         
         return this.each(function () {
+            var img = $(this).find('img'), ln = img.length, $image = 0;
+            def.maxWidth = def.maxWidth - def.padding*2;
             
+            while ($image <= ln) {
+                var width = 0, height = 0, j = $image;
+                
+                while (width < def.maxWidth) {
+                    var lwidth = width, 
+                        iw = $(img[j]).width(),
+                        ih = $(img[j]).height();
+                    width = width + Math.floor(iw/ih)*def.height;
+                    console.log(iw);   
+                    j++; 
+                }
+                $image++;
+            }
         });
     }
 })(jQuery)
